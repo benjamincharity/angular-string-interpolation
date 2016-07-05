9f21907462267cf1d16a7c33ed681d75182f0497
Benjamin Charity <ben@benjamincharity.com>
2016-07-05 15:22:29 -0400

    created interpolate method, added watch for changing array items

--- src/interpolation.controller.js
+++ src/interpolation.controller.js
@@ -1,10 +1,11 @@
 export class InterpolationController {
 
     constructor(
+        $scope
     ) {
         'ngInject';
 
-
+        this.$scope = $scope;
         this._activate();
 
     }
@@ -13,7 +14,39 @@ export class InterpolationController {
 
 
     _activate() {
+        this.string = this.interpolate(this.bcString, this.bcArray);
 
+        // This watch is needed since the items being passed in may not exist yet (async data)
+        this.$scope.$watch('vm.bcArray', (newValue) => {
+            this.string = this.interpolate(this.bcString, this.bcArray);
+        });
+    }
+
+
+    /**
+     * Interpolate text with items from an array
+     *
+     * @param {String} text
+     * @return {String} text (interpolated)
+     */
+    interpolate(text, array) {
+        const regex = /{{(.*?)}}/g;
+        const length = array.length;
+
+        // Loop through the array of replacement items
+        for (let i = 0; i < length; i += 1) {
+            // Replace each matched item
+            text = text.replace(regex, convert)
+        }
+
+        // Custom conversion function
+        function convert(str, p1) {
+            // Grab the content of the match (should be an index number)
+            // Return the array item with the found index
+            return array[p1];
+        }
+
+        return text;
     }
 
 }
--- src/interpolation.directive.js
+++ src/interpolation.directive.js
@@ -1,32 +1,24 @@
-import { InterpolationController } from './.controller';
+import { InterpolationController } from './interpolation.controller';
 
 export function InterpolationDirective(
 ) {
     'ngInject';
 
+    // TODO: Make directive work as an attribute also?
     const directive = {
         restrict: 'E',
         replace: true,
         scope: {},
         bindToController: {
-            bcString: '&',
-            bcArray: '='
+            bcString: '@',
+            bcArray: '=',
         },
-        template: '<span></span>',
-        link: linkFunction,
+        template: '<span data-ng-bind="vm.string"></span>',
         controller: InterpolationController,
         controllerAs: 'vm',
     };
 
     return directive;
-
-
-    /**
-     * Link
-     */
-    function linkFunction($scope, $element, $attrs, vm) {
-
-    }
 
 }
 
