9bd890a9410bb57738e37726da19d1363d330211
Benjamin Charity <ben@benjamincharity.com>
2016-07-08 15:37:17 -0400

    New dist

--- dist/angular-string-interpolation.js
+++ dist/angular-string-interpolation.js
@@ -60,10 +60,69 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var _interpolation2 = __webpack_require__(2);
 	
-	angular.module('bc.AngularStringInterpolation', []).directive('bcInterpolate', _interpolation.InterpolationDirective).controller('InterpolationController', _interpolation2.InterpolationController);
+	var _interpolation3 = __webpack_require__(3);
+	
+	angular.module('bc.AngularStringInterpolation', []).directive('bcInterpolate', _interpolation2.InterpolationDirective).controller('InterpolationController', _interpolation3.InterpolationController).service('bcInterpolationService', _interpolation.InterpolationService);
 
 /***/ },
 /* 1 */
+/***/ function(module, exports) {
+
+	'use strict';
+	
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+	
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+	
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+	
+	var InterpolationService = exports.InterpolationService = function () {
+	    function InterpolationService() {
+	        'ngInject';
+	
+	        _classCallCheck(this, InterpolationService);
+	    }
+	
+	    /**
+	     * Interpolate text with items from an array
+	     *
+	     * @param {String} text Text that contains placeholders
+	     * @return {String} text Interpolated text
+	     */
+	
+	
+	    _createClass(InterpolationService, [{
+	        key: 'interpolate',
+	        value: function interpolate(text, array) {
+	            // Define a regex to look for our custom tag `${}`
+	            var regex = /\${(.*?)}/g;
+	            // Cache the array length
+	            var length = array.length;
+	
+	            // Loop through the array of replacement items
+	            for (var i = 0; i < length; i += 1) {
+	                // Replace each matched item
+	                text = text.replace(regex, convert);
+	            }
+	
+	            // Custom conversion function
+	            function convert(str, p1) {
+	                // Grab the content of the match (should be an index number)
+	                // Return the array item with the found index
+	                return array[p1];
+	            }
+	
+	            return text;
+	        }
+	    }]);
+	
+	    return InterpolationService;
+	}();
+
+/***/ },
+/* 2 */
 /***/ function(module, exports, __webpack_require__) {
 
 	'use strict';
@@ -73,7 +132,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	});
 	exports.InterpolationDirective = InterpolationDirective;
 	
-	var _interpolation = __webpack_require__(2);
+	var _interpolation = __webpack_require__(3);
 	
 	function InterpolationDirective() {
 	    'ngInject';
@@ -95,7 +154,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	}
 
 /***/ },
-/* 2 */
+/* 3 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -109,13 +168,15 @@ return /******/ (function(modules) { // webpackBootstrap
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 	
 	var InterpolationController = exports.InterpolationController = function () {
-	    InterpolationController.$inject = ["$scope"];
-	    function InterpolationController($scope) {
+	    InterpolationController.$inject = ["$scope", "bcInterpolationService"];
+	    function InterpolationController($scope, bcInterpolationService) {
 	        'ngInject';
 	
 	        _classCallCheck(this, InterpolationController);
 	
 	        this.$scope = $scope;
+	        this.bcInterpolationService = bcInterpolationService;
+	
 	        this._activate();
 	    }
 	
@@ -124,41 +185,14 @@ return /******/ (function(modules) { // webpackBootstrap
 	        value: function _activate() {
 	            var _this = this;
 	
-	            this.string = this.interpolate(this.bcString, this.bcArray);
+	            // Interpolated the string and expose to the DOM
+	            this.string = this.bcInterpolationService.interpolate(this.bcString, this.bcArray);
 	
-	            // This watch is needed since the items being passed in may not exist yet (async data)
+	            // This watch is needed since the items being passed in may not exist yet (async data).
+	            // This also allows us to dynamically re-interpolate if any of the array items change.
 	            this.$scope.$watch('vm.bcArray', function (newValue) {
-	                _this.string = _this.interpolate(_this.bcString, _this.bcArray);
+	                _this.string = _this.bcInterpolationService.interpolate(_this.bcString, _this.bcArray);
 	            });
-	        }
-	
-	        /**
-	         * Interpolate text with items from an array
-	         *
-	         * @param {String} text
-	         * @return {String} text (interpolated)
-	         */
-	
-	    }, {
-	        key: 'interpolate',
-	        value: function interpolate(text, array) {
-	            var regex = /\${(.*?)}/g;
-	            var length = array.length;
-	
-	            // Loop through the array of replacement items
-	            for (var i = 0; i < length; i += 1) {
-	                // Replace each matched item
-	                text = text.replace(regex, convert);
-	            }
-	
-	            // Custom conversion function
-	            function convert(str, p1) {
-	                // Grab the content of the match (should be an index number)
-	                // Return the array item with the found index
-	                return array[p1];
-	            }
-	
-	            return text;
 	        }
 	    }]);
 	
